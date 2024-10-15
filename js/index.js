05df7ca5cbaeba09ff9c09160ca26afa3ef54c94
SivaAbinaya <sivaabinaya20@gmail.com>
2024-10-15 18:15:28 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ import {boston} from "./boston.js";
 let data = boston.data;
 let topearners = [];
 
-if(data.salary < 2000000){
-    topearners.push({name:data[0][8],salary : data[0][11]});
- }
+// if(data.salary < 2000000){
+//     topearners.push({name:data[0][8],salary : data[0][11]});
+ // }
 for (let i = 0; i < data.length; i++) {
     if (data[i][11] > 200000) {
         topearners.push({ name: data[i][8], salary: data[i][11] });
@@ -22,7 +22,7 @@ topearners = topearners.slice(0, 5);
  for (let i = 0; i < topearners.length; i++) {
      Earners += `<h2>Top Earner ${i + 1}: ${topearners[i].name} - Salary: ${topearners[i].salary}</h2>`;
  }
-let Earners= `<h1> Name : ${topearners[0].name} <br> Salary : ${topearners[0].salary}`
+// let Earners= `<h1> Name : ${topearners[0].name} <br> Salary : ${topearners[0].salary}`
 document.getElementById("app").innerHTML = Earners;
 console.log(topearners)
 
